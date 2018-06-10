728581bf89bef2003074d5cc590cc7814bd0e540
LuisSas <luis@makingdevs.com>
2018-06-10 15:51:49 -0500

    Put some attributes types in the fruit module

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class FruitComponent{
 	public name_component = 'Componente de fruta';
 	public fruit_list = 'Naranja, Manzana, Pera y Sandia';
+
+	public username:string = "LuisSas";
+	public age:number = 22;
+	public adult:boolean = true;
+	public games:Array<string> = ['Final Fantasy','The Binding of Isaac','Dont Starve'];
+	public list:Array<any> = ['Luis', 22, true];
+	wildcard:any = 23;
 }
