13fb25cfd3a21a2d97b63382a2bb1f9b0ef4387c
LuisSas <luis@makingdevs.com>
2018-06-10 15:59:25 -0500

    Create our first contructor in fruit module

--- src/app/fruit/fruit.component.ts
+++ src/app/fruit/fruit.component.ts
@@ -9,10 +9,18 @@ export class FruitComponent{
 	public name_component = 'Componente de fruta';
 	public fruit_list = 'Naranja, Manzana, Pera y Sandia';
 
-	public username:string = "LuisSas";
-	public age:number = 22;
-	public adult:boolean = true;
+	public username:string;
+	public age:number;
+	public adult:boolean;
 	public games:Array<string> = ['Final Fantasy','The Binding of Isaac','Dont Starve'];
 	public list:Array<any> = ['Luis', 22, true];
 	wildcard:any = 23;
+
+  constructor(){
+  	this.username = "LuisSas";
+  	this.age = 22;
+  	this.adult = true;
+  	console.log("Hola");
+  	console.log(this.games);
+  }
 }
